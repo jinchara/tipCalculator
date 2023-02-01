0224d2816b6d4a93f45857afac7a54e925536e9e
NutsaJ <102469713+NutsaJ@users.noreply.github.com>
2023-02-01 15:55:18 +0400

    application have some bugs

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.core.text.isDigitsOnly
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,8 +22,6 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tipPerson: TextView
     private lateinit var totalPerson : TextView
 
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -31,78 +31,100 @@ class MainActivity : AppCompatActivity() {
         var tip15 = findViewById<Button>(R.id.tip15)
         var tip25 = findViewById<Button>(R.id.tip25)
         var tip50 = findViewById<Button>(R.id.tip50)
+        var tipCustom=findViewById<EditText>(R.id.tipCustom)
 
         numberOfPeople = findViewById(R.id.numberOfPeople)
         amountToBePaid = findViewById(R.id.amountToBePaid)
         tipCustom=findViewById(R.id.tipCustom)
         tipPerson = findViewById(R.id.tipPerson)
         totalPerson=findViewById(R.id.totalPerson)
+        var tip = 0
         tip5.setOnClickListener{
-            val numberOfPeople1 = numberOfPeople.text.toString().toInt()
+            tip = 5
+
+            /*val numberOfPeople1 = numberOfPeople.text.toString().toInt()
             val amountToBePaid1 = amountToBePaid.text.toString().toInt()
-            val tipCustom1 = tipCustom.text.toString().toInt()
-            var tipPerson1 = tipPerson.text.toString().toInt()
-            var totalPerson1 = totalPerson.text.toString().toInt()
-            while (tip5.isActivated  && numberOfPeople.text.isNotEmpty() && amountToBePaid.text.isNotEmpty()){
-                totalPerson.text.toString().toInt() == amountToBePaid1 / numberOfPeople1
-                tipPerson.text.toString().toInt() == totalPerson1 + (amountToBePaid1 * 5 /100 / numberOfPeople1)
+            if (tipCustom.text.isNotEmpty() && tipCustom.text.isDigitsOnly()){
+                val tipCustom1 = tipCustom.text.toString().toInt()
             }
 
+            while (tip5.isActivated  && numberOfPeople.text.isNotEmpty() && amountToBePaid.text.isNotEmpty()){
+                totalPerson.text.toString().toInt() == amountToBePaid1 / numberOfPeople1
+                tipPerson.text.toString().toInt() == (amountToBePaid1 * 5 /100 / numberOfPeople1)
+            }*/
+
+        }
+        tip10.setOnClickListener{
+            tip = 10
+        }
+        tip15.setOnClickListener{
+            tip = 15
+        }
+        tip25.setOnClickListener{
+            tip = 25
+        }
+        tip50.setOnClickListener{
+            tip = 50
         }
 
-/*
+
+
         calculateButton.setOnClickListener{
             val numberOfPeople1 = numberOfPeople.text.toString().toInt()
             val amountToBePaid1 = amountToBePaid.text.toString().toInt()
-            val tipCustom1 = tipCustom.text.toString().toInt()
-            var tipPerson1 = tipPerson.text.toString().toInt()
-            var totalPerson1 = totalPerson.text.toString().toInt()
-
-
-
-
-
             if(numberOfPeople.text.isEmpty()  ){
                 numberOfPeople.setError("Enter number of people")
             }
             if(amountToBePaid.text.isEmpty()){
                 amountToBePaid.setError("Enter the bill")
             }
-            if(tip5.isPressed && numberOfPeople.length() > 0 && amountToBePaid.text.length>0){
-                totalPerson1 == amountToBePaid1 / numberOfPeople1
-                tipPerson1 == totalPerson1 + (amountToBePaid1 * 5 /100 / numberOfPeople1)
-            }
-            if(tip10.isPressed && numberOfPeople.length() > 0 && amountToBePaid.text.length>0){
-                totalPerson1 == amountToBePaid1 / numberOfPeople1
-                tipPerson1 == totalPerson1 + (amountToBePaid1  /10 / numberOfPeople1)
-            }
-            if(tip15.isPressed && numberOfPeople.length() > 0 && amountToBePaid.text.length>0){
-                totalPerson1 == amountToBePaid1 / numberOfPeople1
-                tipPerson1 == totalPerson1 + (amountToBePaid1 * 5 /100 / numberOfPeople1)
+
+
+            when (tip){
+                5->{
+                    totalPerson.text =( amountToBePaid1 / numberOfPeople1).toString()
+                    tipPerson.text =(totalPerson.text.toString().toInt() + (amountToBePaid1  /20 / numberOfPeople1)).toString()
+                }
 
             }
-            if(tip25.isPressed && numberOfPeople.length() > 0 && amountToBePaid.text.length>0){
-                totalPerson1 == amountToBePaid1 / numberOfPeople1
-                tipPerson1 == totalPerson1 + (amountToBePaid1 / 4 / numberOfPeople1)
-            }
-            if(tip50.isPressed && numberOfPeople.length() > 0 && amountToBePaid.text.length>0) {
-                totalPerson1 == amountToBePaid1 / numberOfPeople1
-                tipPerson1 == totalPerson1 + (amountToBePaid1 / 2 / numberOfPeople1)
+            when (tip){
+                10->{
+                    totalPerson.text =( amountToBePaid1 / numberOfPeople1).toString()
+                    tipPerson.text =(totalPerson.text.toString().toInt() + (amountToBePaid1 /10 / numberOfPeople1)).toString()
+                }
 
             }
-            if (tipCustom.isPressed  &&  tipCustom.text.isNotEmpty()){
-                totalPerson1 == amountToBePaid1 / numberOfPeople1
-                tipPerson1 == totalPerson1 + (amountToBePaid1 *tipCustom1 / 100 / numberOfPeople1)
-
+            when (tip){
+                15->{
+                    totalPerson.text =( amountToBePaid1 / numberOfPeople1).toString()
+                    tipPerson.text =(totalPerson.text.toString().toInt() + (amountToBePaid1 * 3 /20 / numberOfPeople1)).toString()
+                }
 
             }
+            when (tip){
+                25->{
+                    totalPerson.text =( amountToBePaid1 / numberOfPeople1).toString()
+                    tipPerson.text =(totalPerson.text.toString().toInt() + (amountToBePaid1  /4 / numberOfPeople1)).toString()
+                }
 
+            }
+            when (tip){
+                50->{
+                    totalPerson.text =( amountToBePaid1 / numberOfPeople1).toString()
+                    tipPerson.text =(totalPerson.text.toString().toInt() + (amountToBePaid1 /2 / numberOfPeople1)).toString()
+                }
 
-
+            }
+            while (tipCustom.text.length > 0){
+                totalPerson.text = ( amountToBePaid1 / numberOfPeople1).toString()
+                tipPerson.text = (totalPerson.text.toString().toInt() + (amountToBePaid1 * totalPerson.text.toString().toInt() / 100 / numberOfPeople1 ) ).toString()
+            }
 
 
         }
-    */
+
 
     }
+
+
 }
